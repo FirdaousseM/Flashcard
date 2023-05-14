258628a55e9dfb168d6d398db931e965a60060f1
hushlovies <canariakaira@gmail.com>
2023-05-14 20:56:29 +0200

    add shuffle in dictionaries.js

--- src/scripts/Dictionnaries.js
+++ src/scripts/Dictionnaries.js
@@ -1,13 +1,22 @@
 // Crée 3 sets aléatoires de 10 mots
 
-const set1 = ["serious", "consciousness", "economics", "resignation", "sticky", "funeral", "reference", "integrity", "integration", "store"];
+const wordSet1 = ["serious", "consciousness", "economics", "resignation", "sticky", "funeral", "reference", "integrity", "integration", "store"];
 
-const set2 = ["abortion", "favorable", "variety", "introduction", "sanctuary", "stress", "remunerate", "play", "side", "seller"];
+const wordSet2 = ["abortion", "favorable", "variety", "introduction", "sanctuary", "stress", "remunerate", "play", "side", "seller"];
 
-const set3 = ["refrigerator", "criticism", "position", "nonremittal", "strip", "profile", "diagram", "mild", "coalition", "rescue"];
+const wordSet3 = ["refrigerator", "criticism", "position", "nonremittal", "strip", "profile", "diagram", "mild", "coalition", "rescue"];
+
+//shuffled arrays:
+
+const set1 = wordSet1.sort(() => Math.random() - 0.5);
+const set2 = wordSet2.sort(() => Math.random() - 0.5);
+const set3 = wordSet3.sort(() => Math.random() - 0.5);
+
 
 export const dictionaries = [
   set1,
   set2,
   set3
 ]
+
+
